3a8c83d940df002d09a165c902a406866efc9fa2
Arundhathi Swami <arundhathi.swami@colorado.edu>
2017-01-25 21:03:36 -0700

    Final_project-Automated_Home_Environment_Control

---
+++ profiling.c
@@ -0,0 +1,37 @@
+/*
+ *  	Filename: profiling.c
+ *      Description:  This source file contains the functions for timer profiling of functions.
+ *
+ *
+ *  Created on: Oct 30, 2016
+ *      Author: Arundhathi Swami, Vignesh Jayaram
+ */
+
+#include "MKL25Z4.h"
+#include "profiling.h"
+#include<stdio.h>
+#include<stdint.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+
+void counter()
+{
+
+	SIM->SCGC6 |= 0x01000000;            //initializing TPM0
+	SIM->SOPT2 |= 0x01000000;           //Selecting FLL clock
+	TPM0->SC = 0x0;                     //Disabling everything
+	TPM0->MOD = 0x0000FFFF;             //Setting Mod value to 65535
+	TPM0->CNT = 0x0;                    //making count 0
+	TPM0->SC = 0X00000080;              //counter overflow
+}
+
+
+double counter_calculate(void)
+{
+	cpu_ticks = TPM0->CNT;
+	time_elapsed= cpu_ticks*CLOCK_RESOLUTION;
+	return time_elapsed;
+}
+
